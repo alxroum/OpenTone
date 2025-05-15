6bdac1f371851b0a032dadf53bef92f087d0f43d
FilthyCashew481 <alxroum@gmail.com>
2025-05-14 21:55:37 -0400

    Inital Project Setup.

---
+++ AudioPlayer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
+using namespace std;
+
+class AudioPlayer
+{
+private:
+    FILE audioFile;
+    FILE configFile;
+public:
+    AudioPlayer(/* args */) { // constructor
+        
+    }
+    ~AudioPlayer() { // deconstructor (unallocate memory)
+
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    
+    cout << "Hello, World!" << endl;
+    return 0;
+}
